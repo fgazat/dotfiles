6f447b86288634929454a251e844e7c7532b6c7a
fgazat <fgazat@yandex.ru>
2025-03-12 14:17:05 +0100

    up

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-ln -s ~/dotfiles/tmux.conf ~/.config/.tmux.conf
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/alacritty ~/.config/alacritty
+ln -sf ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+
 # ln -s ~/dotfiles/shell/aliases ~/.aliases
 # ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
